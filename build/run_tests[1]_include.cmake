61ac184d0739053d7cd5cc147f271a3e194cf06d
Madhur Kaushik <madhurkaushik14@gmail.com>
2025-08-24 11:48:50 +0530

    Initial commit: Added Token-gate rate limiter and tests

---
+++ build/run_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "D:/RATE LIMITER LLD 1.1/build/run_tests[1]_tests.cmake")
+  include("D:/RATE LIMITER LLD 1.1/build/run_tests[1]_tests.cmake")
+else()
+  add_test(run_tests_NOT_BUILT run_tests_NOT_BUILT)
+endif()
